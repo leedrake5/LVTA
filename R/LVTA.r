637b1c900a59b8d9fed020f0bab039f73f8df9e4
Lee Drake <b.lee.drake@gmail.com>
2022-05-10 21:05:33 -0600

    Initialization
    Three plots pulled from a google sheet.

---
+++ R/LVTA.r
@@ -0,0 +1,75 @@
+source("~/GitHub/LVTA/R/back.r")
+
+
+library(ggplot2)
+library(RCurl)
+library(reshape2)
+library(data.table)
+library(gsheet)
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(rvest)
+
+
+
+lvta_main <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit#gid=0", "LVTA")
+finra_nyse <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit#gid=0", "FINRA and NYSE")
+nyse <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit#gid=0", "NYSE")
+finra_1 <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit#gid=0", FINRA1")
+finra_2 <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit#gid=0", "FINRA2")
+
+
+###LVTA
+lvta_data = lvta_main[,c("Date", "LVTA")]
+lvta_data$Date <- as.Date(lvta_data$Date, format="%m/%d/%Y")
+colnames(lvta_data) <- c("Date", "LVTA")
+lvta_data <- lvta_data %>%
+    arrange(Date) %>%
+    mutate(Daily = LVTA - lag(LVTA, default = first(LVTA)))
+
+current_lvta <- ggplot(lvta_data, aes(Date, LVTA)) +
+geom_col(data=lvta_data, mapping=aes(Date, Daily), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth(method="gam") +
+scale_x_date(date_labels = "%m/%d/%Y") +
+scale_y_continuous("Total Debt:Capital", labels=scales::comma) +
+ggtitle(paste0("Total Debt to Capital on ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/LVTA/Plots/lvta_total.jpg", current_lvta, device="jpg", width=6, height=5)
+
+###Free LVTA
+lvta_free_data = lvta_main[,c("Date", "Free_LVTA")]
+lvta_free_data$Date <- as.Date(lvta_free_data$Date, format="%m/%d/%Y")
+colnames(lvta_free_data) <- c("Date", "Free_LVTA")
+lvta_free_data <- lvta_free_data %>%
+    arrange(Date) %>%
+    mutate(Daily = Free_LVTA - lag(Free_LVTA, default = first(Free_LVTA)))
+
+current_lvta_free <- ggplot(lvta_free_data, aes(Date, Free_LVTA)) +
+geom_col(data=lvta_free_data, mapping=aes(Date, Daily), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth(method="gam") +
+scale_x_date(date_labels = "%m/%d/%Y") +
+scale_y_continuous("Free Debt:Capital", labels=scales::comma) +
+ggtitle(paste0("Free Debt to Capital on ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/LVTA/Plots/lvta_free.jpg", current_lvta_free, device="jpg", width=6, height=5)
+
+###Margin LVTA
+lvta_margin_data = lvta_main[,c("Date", "Margin_LVTA")]
+lvta_margin_data$Date <- as.Date(lvta_margin_data$Date, format="%m/%d/%Y")
+colnames(lvta_margin_data) <- c("Date", "Margin_LVTA")
+lvta_margin_data <- lvta_margin_data %>%
+    arrange(Date) %>%
+    mutate(Daily = Margin_LVTA - lag(Margin_LVTA, default = first(Margin_LVTA)))
+
+current_lvta_margin <- ggplot(lvta_margin_data, aes(Date, Margin_LVTA)) +
+geom_col(data=lvta_margin_data, mapping=aes(Date, Daily), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth(method="gam") +
+scale_x_date(date_labels = "%m/%d/%Y") +
+scale_y_continuous("Margin Debt:Capital", labels=scales::comma) +
+ggtitle(paste0("Margin Debt to Capital on ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/LVTA/Plots/lvta_margin.jpg", current_lvta_margin, device="jpg", width=6, height=5)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# LVTA
- Evaluation of debt and capital in financial markets
+# The Leverage Cycle
+
+Based on [Jonn Geanakoplos's work](https://www.journals.uchicago.edu/doi/full/10.1086/648285), and inspired by [this post](https://www.reddit.com/r/investing/comments/um57l8/bursting_a_leveraged_bubble_invest_once_markets/), I though it would be helpful to make this more easily trackable. Data for this project comes from [here](https://www.finra.org/investors/learn-to-invest/advanced-investing/margin-statistics), with the following methodology:
+
+
+> Pursuant to FINRA Rule 4521(d), FINRA member firms carrying margin accounts for customers are required to submit, on a settlement date basis, as of the last business day of the month, the following customer information:
+
+> * the total of all debit balances in securities margin accounts; and
+> * the total of all free credit balances in all cash accounts and all securities margin accounts.1
+> FINRA collects the required data via FINRA's Customer Margin Balance Form. The data is compiled in aggregate form, made available below and in Excel format via the download link on this page. See Margin Balance Reporting: Frequently Asked Questions under FINRA Rule 4521(d) (published April 13, 2021) for additional guidance on the calculation of these balances. Monthly variations in the reported balances may be partially due to member firms modifying the methods used in determining the balances reported to FINRA since the publication of the FAQ. Please note, FINRA generally publishes updates to the Margin Statistics on the third week of the month following the reference month. FINRA does not provide the data outside of this webpage and data feeds are not available.
+
+Data is drawn from [this public google sheet](https://docs.google.com/spreadsheets/d/1Qq2EYhEZeQfszqlYD-lyEsa6VJcGZSq4V9KabMUf2fE/edit?usp=sharing) which is updated monthly. 
+
+## Total Leverage
+![alt text](https://raw.githubusercontent.com/leedrake5/LVTA/master/Plots/lvta_total.jpg?)
+Total debt to both free and margin capital. 
+
+## Free Leverage
+![alt text](https://raw.githubusercontent.com/leedrake5/LVTA/master/Plots/lvta_free.jpg?)
+Total debt to free capital. 
+
+## Margin Leverage
+![alt text](https://raw.githubusercontent.com/leedrake5/LVTA/master/Plots/lvta_margin.jpg?)
+Total debt to margin capital. 
